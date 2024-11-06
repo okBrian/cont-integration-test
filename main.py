7e32d339d4e41445f1759b97879871ad8d709a43
Brian Ok <brianok924@gmail.com>
2024-11-06 14:22:31 -0500

    test

---
+++ main.py
@@ -0,0 +1,20 @@
+import json
+import random
+
+# Generate a random number between 0 and 100
+random_value = random.randint(0, 100)
+
+# Define the JSON structure
+data = [
+    {
+        "name": "My Custom Smaller Is Better Benchmark - CPU Load",
+        "unit": "Percent",
+        "value": random_value
+    }
+]
+
+# Write to a JSON file
+with open('benchmark.json', 'w') as f:
+    json.dump(data, f, indent=4)
+
+print("JSON file 'benchmark.json' created with random value:", random_value)
--- superbsoftware.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <random>
-
-int main() {
-    srand(time(0));
-    std::cout << rand() % 10000 << std::endl;
-    return 0;
-}
