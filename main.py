57ad871366a19f5fc33b082c59b08f6699cc1958
Brian Ok <brianok924@gmail.com>
2024-11-06 15:52:00 -0500

    test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import random
+import gspread
 
 # Generate a random number between 0 and 100
 random_value = random.randint(0, 100)
@@ -10,11 +11,14 @@ data = [
         "name": "My Custom Smaller Is Better Benchmark - CPU Load",
         "unit": "Percent",
         "value": random_value
+    },
+    {
+        "name": "hallo",
+        "unit": "asd",
+        "value": random_value % 50
     }
 ]
   
 # Write to a JSON file
 with open('benchmark.json', 'w') as f:
     json.dump(data, f, indent=4)
-
-print("JSON file 'benchmark.json' created with random value:", random_value)
